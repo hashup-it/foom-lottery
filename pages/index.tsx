72649c748f5e6dccc38181a8bcd4ec4e1f9eb47c
hashup-dev <hashup.it@gmail.com>
2025-05-07 16:08:05 +0200

    feat: add wallet connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,17 @@ import { _log, _warn } from '@/utils/ts'
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <div>header</div>
-      <div className="w-full flex items-center justify-start flex-col">content</div>
-      <div>&copy; FOOM AI corporation 2025</div>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex">
+        <p>header</p>
+      </div>
+      <div className="w-full flex items-center justify-start flex-col gap-2">
+        <h1 className="text-2xl">FOOM Lottery</h1>
+        <appkit-button />
+      </div>
+      <div className="flex-grow flex items-end justify-center">
+        <p>&copy; FOOM AI corporation 2025</p>
+      </div>
     </div>
   )
 }
