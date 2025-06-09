71aaf6fc304754d232c366ea5d8abcd35f6bae0f
hashup-dev <hashup.it@gmail.com>
2025-06-09 14:04:22 +0200

    feat: use 1080/1081 ports for foom-2 server

--- compose-context.sh
+++ compose-context.sh
@@ -3,11 +3,11 @@
 IS_STAGING=${IS_STAGING:-false}
 
 if [ "$IS_STAGING" = "true" ]; then
-  export HOST_PORT=1080
+  export HOST_PORT=1081
   export CONTAINER_NAME="foom-lottery-staging"
   export COMPOSE_PROJECT_NAME="foom_lottery_staging"
 else
-  export HOST_PORT=80
+  export HOST_PORT=1080
   export CONTAINER_NAME="foom_lottery"
   export COMPOSE_PROJECT_NAME="foom_lottery"
 fi
